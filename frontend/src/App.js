31b029a0e90f56a524667c8a555609737052cfe5
chevalmuscle <me@marcantoinejean.com>
2020-06-21 15:05:06 -0400

    added basic frontend-backend http comm. with axios

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,20 @@
 import React from "react";
+import axios from "axios";
 
-function App() {
-  return (
-    <div>
-      <h1>Hello world</h1>
-    </div>
-  );
+import { BACKEND_URL } from "./config";
+
+export default class App extends React.Component {
+  state = {
+    helloWord: undefined,
+  };
+
+  componentDidMount() {
+    axios.get(BACKEND_URL).then((res) => {
+      this.setState({ helloWord: res.data });
+    });
+  }
+
+  render() {
+    return <div>{this.state.helloWord}</div>;
+  }
 }
-
-export default App;

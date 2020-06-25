2fee79d27a7cd977da3abf3bd4435c3feda05d32
chevalmuscle <me@marcantoinejean.com>
2020-06-25 18:57:16 -0400

    added decorators to organization entity's fields

--- backend/src/organizations/organization.dto.ts
+++ backend/src/organizations/organization.dto.ts
@@ -1,10 +1,36 @@
+import { IsEmail, IsMobilePhone, IsNotEmpty, IsString, Length, MaxLength } from 'class-validator';
+
 export class OrganizationDto {
+  @Length(10, 10)
+  @IsString()
   EIN: string;
+  
+  @IsNotEmpty()
+  @IsString()
   name: string;
+  
+  @IsNotEmpty()
+  @MaxLength(1000)
+  @IsString()
   description: string;
+
+  @IsMobilePhone('en-US')
   phoneNumber: string;
+  
+  @IsEmail()
   email: string;
+  
+  @Length(8, 128)
+  @IsString()
   password: string;
+  
+  @IsNotEmpty()
+  @MaxLength(100)
+  @IsString()
   contactFirstName: string;
+  
+  @IsNotEmpty()
+  @MaxLength(100)
+  @IsString()
   contactLastName: string;
 }

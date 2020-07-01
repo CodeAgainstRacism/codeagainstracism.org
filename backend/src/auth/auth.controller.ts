c71332dbf1e273530131a9e857fbfaf42d156f50
chevalmuscle <me@marcantoinejean.com>
2020-07-01 19:42:25 -0400

    added swagger documentation to auth controller

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -5,14 +5,23 @@ import {
   HttpException,
   HttpStatus,
 } from '@nestjs/common';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { AuthService } from './auth.service';
 import { JwtPayload } from './interfaces/jwt-payload.interface';
 
 @Controller('auth')
+@ApiTags('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
   @Post('login')
+  @ApiOperation({ summary: 'Logs in as an organization' })
+  @ApiResponse({ status: 201, description: 'Sends the accessToken and the corresponding organization' })
+  @ApiResponse({
+    status: 401,
+    description:
+      'Unauthorized access. The email and password combination do not match or do not exist.',
+  })
   async login(@Body() payload: JwtPayload): Promise<any> {
     const organization = await this.authService.validateOrganization(payload);
 

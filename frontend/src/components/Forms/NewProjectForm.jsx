005f1d7b54cfce17e2f83d76a5d4576e7072f8a0
fluffysheepcloud <petrichormist581@gmail.com>
2020-07-24 14:27:14 -0700

    committing newprojectform and styles.scss

--- frontend/src/components/Forms/NewProjectForm.jsx
+++ frontend/src/components/Forms/NewProjectForm.jsx
@@ -1,80 +1,112 @@
 import React, { Fragment } from "react";
+import {Button} from "@material-ui/core";
+import {TextField} from "@material-ui/core";
+import { FormLabel } from '@material-ui/core';
+import { withStyles } from '@material-ui/core/styles';
+import MyVert from "./VerticalNavBar";
 
-const programmingLanguages = [
-  "HTML/CSS",
-  "JavaScript",
-  "TypeScript",
-  "C",
-  "C++",
-  "Java",
-  "Python",
-  "C#",
-  "R",
-  "PHP",
-  "Swift",
-  "Objective-C",
-  "SQL",
-  "Go",
-  "Ruby",
-  "Shell",
-];
+const StyledField = withStyles({
+  root: {
+    width: '500px',
+  },
+})(TextField);
 
 class NewProjectForm extends React.Component {
   render() {
     return (
-      <div className="container">
+      <div className = "container">
+      <div className="inner-container">
+        
+          <MyVert s/>
+        
+        <div className= "rightCon">
+          <div className = "headingCon">
+              <h1 className="form-title">Create A New Project</h1>
+              <label>───────────────────────────────────────────────────────────────────────────────────────</label>
+          </div>
         <form className="form-container">
-          <h1 className="form-title">Create A New Project</h1>
-          <div>
-            <label htmlFor="projectName">Name of Project:</label>
-            <input type="text" className="projectName" />
+          
+          
+          <div className = "labelCon">
+            <label htmlFor="projectName">Project Name: </label>
           </div>
+          <div className = "textRight">
+            <StyledField id = "outlined-basic" label = "Enter Your Project's Name" variant = "outlined" row = '5' size = "small"/>
+          </div>
+          
+          
+          <div className = "labelCon">
+            <label htmlFor="description"> Description of Project: </label> 
+          </div>
+          <div className = "textRight">
+            <StyledField id = "outlined-basic"  label = "Tell us about your project! Be sure to include details of your organization, requirements for potential members, and any further suggestions" rows = "20" columns = "30" variant = "outlined" multiline = {true}/>
+          </div>
+          
           <div className="flex-box">
-            <label htmlFor="projectStartDate"> Start Date:</label>
-            <input type="date" className="projectStartDate" />
-            <label htmlFor="projectEndDate"> End Date:</label>
-            <input type="date" className="projectEndDate" />
+          
+          <div className = "labelCon"> 
+            <label htmlFor="projectEndDate"> Due Date: </label>
           </div>
+          <div className = "textRight">
+            <TextField id = "outlined-basic" type = "date" variant = "outlined" size = "small"/>
+          </div>
+         
+          </div>
+         
+         
+
+          <Button
+            variant="contained"
+            component="label"
+            color = "primary"
+
+          >
+            Upload File
+          <input
+            type="file"
+            style={{ display: "none" }}
+          />
+          </Button>
+
+
+
+
+
+
           <div>
-            <label htmlFor="languages"> Type of Language:</label>
-            <div className="checkbox-container">
-              {programmingLanguages.map((language) => (
-                <div key={language} className="checkbox">
-                  <label htmlFor={"checkbox_" + language}>{language}</label>
-                  <input
-                    // key={language}
-                    id={"checkbox_" + language}
-                    type="checkbox"
-                    name={"checkbox_" + language}
-                    value={language}
-                  />
-                </div>
-              ))}
+            <label>────────────────────────────────────────────────────────────────────────</label>
+            <h2>Contact Information</h2>
+            
+            
+            
+            <div className = "labelCon">
+            <label htmlFor="contactName">Name: </label>
             </div>
-          </div>
-          <div>
-            <input
-              type="text"
-              name="other-languages"
-              placeholder="Enter new languages"
-            />
-            <button>Submit</button>
-          </div>
-          <div>
-            <label htmlFor="description"> Description of Project:</label>
-            <textarea name="description" id="description" rows="5" />
-          </div>
-          <div>
-            <h4> Point of Contact</h4>
-            <label htmlFor="contactName">Name:</label>
-            <input type="text" className="contactName" />
-            <label htmlFor="contactPhoneNumber">Phone (optional):</label>
-            <input type="text" className="contactPhoneNumber" />
+            <div className = "textRight">
+            <StyledField id = "outlined-basic" variant = "outlined" row = '5' size = "small"/>
+            </div>
+            
+            
+            <div className = "labelCon">
+            <label htmlFor="contactPhoneNumber">Phone(optional): </label>
+            </div>
+            <div className = "textRight">
+            <StyledField id = "outlined-basic" variant = "outlined" row = '5' size = "small"/>
+            </div>
+            
+            
+            <div className = "labelCon">
             <label htmlFor="contactEmail">Email:</label>
-            <input type="email" className="contactEmail" />
+            </div>
+            <div className = "textRight">
+            <StyledField id = "outlined-basic" variant = "outlined" row = '5' size = "small"/>
+            </div>
+            
           </div>
-          <button className="submit-button">Submit</button>
+          <Button color ="primary" variant = "contained">Submit</Button>
         </form>
+        </div>
+      </div>
       </div>
     );
   }

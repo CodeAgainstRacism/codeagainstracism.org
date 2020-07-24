1e84bda871f90565647f86e2e2e642dab3f377e2
fluffysheepcloud <petrichormist581@gmail.com>
2020-07-24 14:32:28 -0700

    committing rest of the files

--- frontend/src/components/Forms/NewOrganizationForm.jsx
+++ frontend/src/components/Forms/NewOrganizationForm.jsx
@@ -6,9 +6,4 @@ const NewOrganizationForm = () => (
     </div>
 )
 
-<<<<<<< HEAD
 export default NewOrganizationForm;
-=======
-export default NewOrganizationForm;
-
->>>>>>> a53ff1dee3614355503f2e36f50cd12cfa71aa2e

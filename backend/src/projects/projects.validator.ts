2688e714a10a2840c7558b922fea2aeb8b0c8ef2
mello-code <48005787+mello-code@users.noreply.github.com>
2020-08-04 13:27:12 -0700

    Revert "http status code fix"
    This reverts commit d973374dda5d1b30b79624fd5e6bc1d42e071536.

--- backend/src/projects/projects.validator.ts
+++ backend/src/projects/projects.validator.ts
@@ -1,16 +1,13 @@
-import { registerDecorator, ValidationOptions, ValidationArguments } from "class-validator";
-import { ValidOptions } from "./projects.validoptions";
+import {registerDecorator, ValidationOptions, ValidationArguments} from "class-validator";
 
-export function IsValidDate(property: string, ValidOptions?: ValidationOptions) {
-    errorHttpStatusCode: 442;
-
-    return function (object: Object, propertyName: string) {
+export function IsValidDate(property: string, validationOptions?: ValidationOptions) {
+   return function (object: Object, propertyName: string) {
         registerDecorator({
             name: "IsValidDate",
             target: object.constructor,
             propertyName: propertyName,
             constraints: [property],
-            options: ValidOptions,
+            options: validationOptions,
             validator: {
                 validate(value: any, args: ValidationArguments) {
                     const [relatedPropertyName] = args.constraints;
--- backend/src/projects/projects.validoptions.ts
+++
@@ -1,8 +0,0 @@
-import { ValidationError, ValidatorOptions } from "class-validator";
-
-export interface ValidOptions extends ValidatorOptions {
-    transform?: boolean;
-    disableErrorMessages?: boolean;
-    errorHttpStatusCode: 442;
-    exceptionFactory?: (errors: ValidationError[]) => any;
-}

b1ec00bff5d4fba67347422c428de4eaf2233064
fluffysheepcloud <petrichormist581@gmail.com>
2020-08-05 15:31:02 -0700

    made newprojectform more responsive

--- frontend/src/pages/NewProjectForm.jsx
+++ frontend/src/pages/NewProjectForm.jsx
@@ -15,59 +15,69 @@ const projectFormStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
   },
+
+  dividerColor: {
+    backgroundColor: 'black',
+  },
+
   paper: {
     padding: theme.spacing(1, 2), 
     textAlign: 'left',
     color: theme.palette.text.primary,
-    },
+    fontSize: "16px"
+  },
+
+  headerPaper: {
+    padding: theme.spacing(1, 2), 
+    textAlign: 'left',
+    color: theme.palette.text.primary,
+    background: "#f2f2f2"
+  },
+
+  outerPaper:{
+    padding: theme.spacing(0.5, 0, 0, 0), 
+    backgroundColor: "#f2f2f2"
+  },
 
   pageContainer: {
-    //height: "768px",
-    width: "1366px",
+    height: "60 vmax",
     margin: "0 auto",
     textAlign: "center",
-    background: "#f2f2f2",
+    backgroundColor: theme.palette.background,
     overflow: "hidden",
-    maxWidth: "70vw", //TODO check if needed
+    maxWidth: "70vw", 
   },
 
   innerContainer: {
-    //width: "1366px",
-    margin: "20 rem",
-    border: "1px solid #292929",
+    //border: "1px solid #292929",
+    //boxSizing: "border-box",
     alignItems: "stretch",
-    boxSizing: "border-box",
-    background: "#f2f2f2",
+    backgroundColor: theme.palette.background,
     overflow: "hidden", 
     display: "flex",
     justifyContent: "center",
-    
-    
   },
 
   headingContainer: {
-    border: "1px solid #292929",
-    boxSizing: "border-box",
-    textAlign: "left"
+    // border: "1px solid #292929",
+    // boxSizing: "border-box",
+    textAlign: "left",
+    backgroundColor: theme.palette.background
   },
 
-  sideBarContainer: {
-    border: "1px solid #292929",
-    boxSizing: "border-box",
-    marginLeft: "200px",
-    display: "inline"
+  formContainer: {
+    backgroundColor: "white",
+    width: "100%"
   },
 
   rightContainer: {
   //contains the heading and the form 
+    width: "100%",
     backgroundColor: "white",
-    border: "1px solid #292929",
-    boxSizing: "border-box",
-    marginTop: "20px",
-    marginBottom: "6 rem",
-    marginLeft: "20px",
-    maxWidth: "650px",
-    //marginRight: "200px"
+    // border: "1px solid #292929",
+    // boxSizing: "border-box",
+    marginLeft: "2%",
+    paddingRight: "1%"
   },
 }))
 export default function NewProjectForm() { 
@@ -78,19 +88,28 @@ export default function NewProjectForm() {
       <React.Fragment>
         <CssBaseline />
           <Container className = {classes.pageContainer}>
-            <Container className={classes.innerContainer}>
-              <SideBar/>
+            <Container disableGutters = {true} className={classes.innerContainer}>
+            <div className={classes.root}>
+              <Grid container spacing={0}>
+                <Grid item xs={3}>
+                <Paper elevation = {0} className={classes.outerPaper}>
+                  <SideBar/>
+                </Paper>
+                </Grid>
+              <Grid item xs = {9}>
+              <Paper elevation = {0} className={classes.outerPaper}>
               <Container disableGutters = {true} className= {classes.rightContainer}>
                 <div className={classes.root}>
                   <Grid container spacing={0}>
-                    <Container className = {classes.headingContainer}> 
+                    <Container disableGutters = {true} className = {classes.headingContainer}> 
                       <Grid item xs={12}>
-                        <Paper elevation = {0} className={classes.paper}>
+                        <Paper elevation = {0} className={classes.headerPaper}>
                           <h1>Create A New Project</h1>
-                          <Divider variant="fullwidth"/>
+                          <Divider classes={{root: classes.dividerColor}} variant="fullwidth"/>
                         </Paper>
                       </Grid>
                     </Container>
+                    {/*<Container disableGutters = {true} className = {classes.formContainer}>*/}
                     <Grid item xs = {12} sm = {12}>
                       <Paper elevation = {0} className={classes.paper}>
                         <TextField placeholder = "Enter Your Project's Name"/>
@@ -98,23 +117,26 @@ export default function NewProjectForm() {
                     </Grid>
                     <Grid item xs = {12} sm = {12}>
                       <Paper elevation = {0} className={classes.paper}>
-                        <TextField placeholder = "Tell us about your project! Be sure to include details of your organization, requirements for potential members, and any further suggestions" rows = "20" columns = "30" multiline = {true}/>
+                        <TextField placeholder = "Tell us about your project! Be sure to include details of your organization, requirements for potential members, and any further suggestions" 
+                        rows = "15" columns = "30" multiline = {true}/>
                       </Paper>
                     </Grid>
-                    <Grid item xs = {12} sm = {4}>
+                    <Grid item xs = {12} sm = {3}>
                       <Paper elevation = {0} className={classes.paper}> 
                         Due Date: 
                       </Paper>
                     </Grid>
-                    <Grid item xs = {12} sm = {8}>
+                    <Grid item xs = {12} sm = {9}>
                       <Paper elevation = {0} className={classes.paper}>
-                        <TextField type = "date"/>
+                        <TextField fullWidth = {false} type = "date"/>
                       </Paper>
                     </Grid>
-                    <Grid item xs = {12} sm = {4}>
-                      <Paper elevation = {0} className={classes.paper}></Paper>
+                    <Grid item xs = {12} sm = {3}>
+                      <Paper elevation = {0} className={classes.paper}>
+                        Attachments:
+                      </Paper>
                     </Grid>
-                    <Grid item xs = {12} sm = {8}>
+                    <Grid item xs = {12} sm = {9}>
                       <Paper elevation = {0} className={classes.paper}> 
                         <Button variant="contained" component="label" color = "primary"> 
                           Upload File
@@ -128,57 +150,63 @@ export default function NewProjectForm() {
                         <h2>Contact Information</h2>
                       </Paper>
                     </Grid>
-                    <Grid item xs = {12} sm = {4}>
+                    <Grid item xs = {12} sm = {3}>
                       <Paper elevation = {0} className={classes.paper}> 
                         Name: 
                       </Paper> 
                     </Grid>
-                    <Grid item xs = {12} sm = {8}>
+                    <Grid item xs = {12} sm = {9}>
                       <Paper elevation = {0} className={classes.paper}> 
-                        <TextField/>
+                        <TextField placeholder = "No name has been given" />
                       </Paper> 
                     </Grid>
-                    <Grid item xs = {12} sm = {4}>
+                    <Grid item xs = {12} sm = {3}>
                       <Paper elevation = {0} className={classes.paper}> 
-                        Phone(optional): 
+                        Phone (optional): 
                       </Paper>
                     </Grid>
-                    <Grid item xs = {12} sm = {8}>
+                    <Grid item xs = {12} sm = {9}>
                         <Paper elevation = {0} className={classes.paper}> 
-                          <TextField/>              
+                          <TextField placeholder = "No phone number has been given"/>              
                         </Paper>
                     </Grid>
-                    <Grid item xs = {12} sm = {4}>
+                    <Grid item xs = {12} sm = {3}>
                       <Paper elevation = {0} className={classes.paper}> 
                         Email:
                       </Paper>
                     </Grid>
-                    <Grid item xs = {12} sm = {8}>
+                    <Grid item xs = {12} sm = {9}>
                       <Paper elevation = {0} className={classes.paper}> 
-                        <TextField/>              
+                        <TextField placeholder = "No email has been given"/>              
                       </Paper>
                     </Grid>
-                    <Grid item xs = {12} sm = {4}>
+                    <Grid item xs = {12} sm = {3}>
                       <Paper elevation = {0} className={classes.paper}> 
                         Primary organization:
                       </Paper>
                     </Grid>
-                    <Grid item xs = {12} sm = {8}>
+                    <Grid item xs = {12} sm = {9}>
                       <Paper elevation = {0} className={classes.paper}>
-                        <TextField/> 
+                        <TextField placeholder = "No organization has been given"/> 
                       </Paper>
                     </Grid>
-                    <Grid item xs = {12} sm = {4}>
+                    <Grid item xs = {12} sm = {5}>
                       <Paper elevation = {0} className={classes.paper}></Paper>
                     </Grid>
-                    <Grid item xs = {12} sm = {8}>
+                    <Grid item xs = {12} sm = {7}>
                       <Paper elevation = {0} className={classes.paper}>
                         <Button color ="primary" variant = "contained">Submit</Button>
                       </Paper>
                     </Grid> 
+                    {/*</Container>*/}
                   </Grid>
                 </div>
               </Container>
+              </Paper>
+              </Grid>
+              
+              </Grid>
+              </div>
             </Container>
           </Container>
         {/* Footer */}
--- frontend/src/pages/VerticalNavBar.jsx
+++ frontend/src/pages/VerticalNavBar.jsx
@@ -2,35 +2,71 @@ import React from "react";
 import { makeStyles } from "@material-ui/styles";
 import {Container} from "@material-ui/core";
 import Divider from '@material-ui/core/Divider';
-//maybe use chips for styling
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import AccountBoxIcon from '@material-ui/icons/AccountBox';
+import FolderIcon from '@material-ui/icons/Folder';
+import CreateIcon from '@material-ui/icons/Create';
+import GroupIcon from '@material-ui/icons/Group';
+import PersonIcon from '@material-ui/icons/Person';
+
 //use styling hook here to align name and png next to each other
 const sideBarStyles = makeStyles((theme)  => ({
     sideBarContainer: {
-        width: "15rem",
+        width: "100%",
+        height: "120vh",
+        paddingTop: "15%",
         //height: "768px",
-        textAlign: "center",
-        backgroundColor: "white"  
-    }
+        //textAlign: "left",
+        alignItems: "center",
+        backgroundColor: "#ffffff"  
+    },
 }));
 
 
 export default function SideBar(){
    const classes = sideBarStyles();
     return(
-        <Container className = {classes.sideBarContainer}>
-            
-                <div>
-                <img src={'avatar.png'}/> 
-                <h3>Name</h3>
-                </div>
-                    
-           
-            <Divider variant="fullwidth"/>
-            <h3>Your Projects</h3>
-            <Divider variant="fullwidth"/>
-            <h3>Account Details</h3>
-            <Divider variant="fullwidth"/>
-            <h3>Create Projects</h3>
+
+        <Container disableGutters = {true} className = {classes.sideBarContainer}>
+            <Container>
+                {/*<img src = {"avatar.png"}/> */}
+                <PersonIcon fontSize="large" />
+                <h3>Code Against Racism of the United States of Americas</h3>
+            </Container>
+            <Divider/>
+            <List>
+                <ListItem button>
+                    <ListItemIcon>
+                        <AccountBoxIcon />
+                    </ListItemIcon>
+                    <ListItemText primary = "Account Details" />
+                </ListItem>
+                <Divider/>
+                <ListItem button>
+                    <ListItemIcon>
+                        <CreateIcon />
+                    </ListItemIcon>
+                    <ListItemText primary = "Create Project" />
+                </ListItem>
+                <Divider/>
+                <ListItem button>
+                    <ListItemIcon>
+                        <FolderIcon />
+                    </ListItemIcon>
+                    <ListItemText primary = "Your Projects" />
+                </ListItem>
+                <Divider/>
+                <ListItem button>
+                    <ListItemIcon>
+                        <GroupIcon />
+                    </ListItemIcon>
+                    <ListItemText primary = "Your Teams" />
+                </ListItem>  
+                <Divider/>
+            </List>
         </Container>
     )
 }

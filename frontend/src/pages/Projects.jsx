d1057a850ce0c8c42d565f042e99d41ddb977eca
Dustin Cai <dvcai@ucdavis.edu>
2020-08-23 22:31:03 -0700

    Synced live search with pagination and stuck footer to the bottom of the page.

--- frontend/src/pages/Projects.jsx
+++ frontend/src/pages/Projects.jsx
@@ -1,4 +1,4 @@
-import React, { Fragment, useEffect, useState} from "react";
+import React, { Fragment, useEffect, useState} from 'react';
 import {
   Box,
   Container,
@@ -6,39 +6,40 @@ import {
   makeStyles,
   Typography,
   InputBase,
-} from "@material-ui/core";
+} from '@material-ui/core';
 import SearchIcon from '@material-ui/icons/Search';
-import { Pagination } from "@material-ui/lab";
-import ProjectCard from "../components/ProjectCard";
+import { Pagination } from '@material-ui/lab';
+import ProjectCard from '../components/ProjectCard';
 import axios from 'axios';
-import Footer from "../components/Footer";
+import Footer from '../components/Footer';
 
 
 const useStyles = makeStyles((theme) => ({
   contentStyle: {
-    display: "flex",
-    justifyContent: "center",
+    display: 'flex',
+    justifyContent: 'center',
   },
   marginStyle: {
     marginTop: theme.spacing(3),
   },
   dividerContainer: {
-    display: "flex",
-    alignItems: "center",
+    display: 'flex',
+    alignItems: 'center',
     marginBottom: theme.spacing(5),
   },
   projectTitle: {
-    alignText: "center",
-    display: "inline",
+    alignText: 'center',
+    display: 'inline',
     padding: theme.spacing(0, 5),
+    whiteSpace: 'nowrap',
   },
   line: {
-    border: "none",
+    border: 'none',
     borderTop: `3px solid ${theme.palette.text.primary}`,
     color: theme.palette.text.secondary,
-    overflow: "visible",
-    textAlign: "center",
-    width: "50%",
+    overflow: 'visible',
+    textAlign: 'center',
+    width: '50%',
   },
   input: {
     marginLeft: theme.spacing(1),
@@ -46,82 +47,85 @@ const useStyles = makeStyles((theme) => ({
     backgroundColor: theme.palette.common.white,
   },
   inputStyle: {
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
-    textAlign: "center",
-    width: "25%",
-    backgroundColor: "white",
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    textAlign: 'center',
+    width: '25%',
+    backgroundColor: 'white',
   },
   iconStyle: {
     paddingLeft: theme.spacing(0.5),
   },
-  bodyStyle: {
-    margin: theme.spacing(0),
-    minHeight: "100vh",
-    width: "100vh",
+  bodyStyle: { // may delete
+    minHeight: '100vh',
+    width: '100vh',
     marginTop: theme.spacing(3),
   },
   paginationStyle: {
-    marginBottom: theme.spacing(2),
-    display: "flex",
-    justifyContent: "center",
+    margin: theme.spacing(4),
+    display: 'flex',
+    justifyContent: 'center',
+  },
+  footerStyle: {
+    flexShrink: 0,
+  },
+  viewStyle: {
+    minHeight: '100vh',
   }
 }));
 
 export default function Projects() {
   const classes = useStyles();
-  const cardsPerPage = 9;
   const [projectList, setProjectList] = useState([]);
-  const [searchedCard, setSearchedCard] = useState("");
+  const [searchedCard, setSearchedCard] = useState('');
   const [currentPage, setCurrentPage] = useState(1);
   const [totalPages, setTotalPages] = useState(1);
+  const [cardsPerPage] = useState(9);
   const indexOfLastCard = currentPage * cardsPerPage;
   const indexOfFirstCard = indexOfLastCard - cardsPerPage;
   const currentCards = projectList.slice(indexOfFirstCard, indexOfLastCard);
 
   useEffect(()=>{
     axios.get('http://ec2-3-23-105-141.us-east-2.compute.amazonaws.com:4000/projects', {
-    params: {
-      ID: 12345
-    }
-    })
-    .then(function (response) {
-      console.log(response);
-      if(response.status === 200){
-        const { data } = response;
-        const newCards = [];
-        if(searchedCard === ""){
-          data.forEach((card, index) => {
-            newCards[index] = {
-              id: card.id,
-              name: card.name,
-              description: card.description,
-              index: index,
-            };
-          });
-        } else { // if search field is not empty
-          data.forEach((card, index) => {
-            if(card.name.toLowerCase().includes(searchedCard)){
+        params: {}
+      })
+      .then(function (response) {
+        if(response.status === 200){
+          const { data } = response;
+          const newCards = [];
+          if(searchedCard === ''){
+            data.forEach((card, index) => {
               newCards[index] = {
                 id: card.id,
                 name: card.name,
                 description: card.description,
                 index: index,
               };
-            }
-          });
+            });
+          } else {
+            data.forEach((card, index) => {
+              if(card.name.toLowerCase().includes(searchedCard)){
+                newCards[index] = {
+                  id: card.id,
+                  name: card.name,
+                  description: card.description,
+                  index: index,
+                };
+              }
+            });
+            setCurrentPage(1);  // Setting the pagination back to the first page
+          }
+          setProjectList(newCards);
+          setTotalPages(Math.ceil(projectList.length / cardsPerPage));
         }
-        setProjectList(newCards);
-        setTotalPages(Math.ceil(projectList.length / 9));
-      }
-    })
-    .catch(function (error) {
-      console.log(error);
-    })
+      })
+      .catch(function (error) {
+        console.log(error);
+      })
   });
 
-  const handleSearch = (event) => {
+  const handleSearch = event => {
     setSearchedCard(event.target.value);
   }
 
@@ -137,51 +141,57 @@ export default function Projects() {
     );
   }
 
+  const getPagination = () => {
+    return (
+      projectList.length > 0 &&
+      <Pagination
+        count={totalPages}
+        page={currentPage}
+        variant='outlined'
+        shape='rounded'
+        onChange={handleChangePage}
+      />
+    );
+  }
+
   return (
-    <Fragment>
-      <Container maxWidth="lg" direction="column">
-        <Grid container direction="column" className={classes.contentStyle}>
-          <Grid item container direction="column" className={classes.contentStyle}>
-            <Grid item className={classes.marginStyle}>
-              <Box className={classes.dividerContainer}>
-                <hr className={classes.line}/>
-                <Typography
-                  variant="h4"
-                  align="center"
-                  gutterBottom
-                  className={classes.projectTitle}
-                >
-                  All Projects
-                </Typography>
-                <hr className={classes.line}/>
-              </Box>
-              <div className={classes.contentStyle}>
-                <div className={classes.inputStyle}>
-                  <SearchIcon className={classes.iconStyle}/>
-                  <InputBase
-                    className={classes.input}
-                    placeholder="Search for a project"
-                    inputProps={{ 'aria-label': 'search for a project' }}
-                    onChange={handleSearch}
-                  />
-                </div>
+    <Fragment >
+      <Container maxWidth='lg' direction='column' className={classes.viewStyle}>
+        <Grid container direction='column' className={classes.contentStyle}>
+          <Grid item className={classes.marginStyle}>
+            <Box className={classes.dividerContainer}>
+              <hr className={classes.line}/>
+              <Typography
+                variant='h4'
+                align='center'
+                gutterBottom
+                className={classes.projectTitle}
+              >
+                All Projects
+              </Typography>
+              <hr className={classes.line}/>
+            </Box>
+            <div className={classes.contentStyle}>
+              <div className={classes.inputStyle}>
+                <SearchIcon className={classes.iconStyle}/>
+                <InputBase
+                  className={classes.input}
+                  placeholder='Search for a project'
+                  inputProps={{ 'aria-label': 'search for a project' }}
+                  onChange={handleSearch}
+                />
               </div>
-            </Grid>
-            <Grid item container spacing={3} className={classes.bodyStyle}>
-              {currentCards.map(projectCardObj => getProjectCards(projectCardObj))}
-            </Grid>
-            <div className={classes.paginationStyle}>
-              <Pagination
-                count={totalPages}
-                variant="outlined"
-                shape="rounded"
-                onChange={handleChangePage}                
-              />
             </div>
           </Grid>
+          <Grid item container spacing={3} className={classes.marginStyle}>
+            {currentCards.map(projectCardObj => getProjectCards(projectCardObj))}
+          </Grid>
+          <div className={classes.paginationStyle}>
+            {getPagination()}
+          </div>
         </Grid>
       </Container>
-      <Footer />
+      <Footer className={classes.footerStyle}/>
     </Fragment>
   );
 }

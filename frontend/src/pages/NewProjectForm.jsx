e250c8d3ab6a1c7c4dbfa9088c2f0e68c8977034
fluffysheepcloud <petrichormist581@gmail.com>
2020-08-29 15:08:44 -0700

    got rid of paper and changed to using grid area

--- frontend/src/pages/NewProjectForm.jsx
+++ frontend/src/pages/NewProjectForm.jsx
@@ -1,91 +1,74 @@
 import React from "react";
 import {makeStyles } from '@material-ui/core';
-import Footer from "../components/Footer";
 import {Container,
         CssBaseline,
         Button,
         TextField,
-        Paper,
-        Grid,} from '@material-ui/core';
+        Grid,
+        Box} from '@material-ui/core';
 import Divider from '@material-ui/core/Divider';
-import SideBar from "../components/SideBarOrganization";
+import Footer from "../components/Footer";
+//for the sidebar, title, and form try using grid area
 //checkut form authentification 
-//create label fields for name and description
-//make padding between textfields more bigger?
-//make sidebar their own branch
 const projectFormStyles = makeStyles((theme) => ({
 
   root: {
     flexGrow: 1,
   },
 
-  dividerColor: {
-    backgroundColor: 'black',
-  },
-
-  paper: {
-    padding: theme.spacing(1, 2), 
-    textAlign: 'left',
-    color: theme.palette.text.primary,
-    fontSize: "16px"
-  },
-
-  headerPaper: {
-    padding: theme.spacing(1,0,1,0), 
-    textAlign: 'left',
-    color: theme.palette.text.primary,
-    backgroundColor: "#f2f2f2",
-    borderRadius : "0"
-
-  },
-
-  outerPaper:{
-    padding: theme.spacing(0, 0, 0, 0), 
-    backgroundColor: "#f2f2f2"
-  },
-
-  pageContainer: {
-    height: "60 vmax",
-    textAlign: "center",
-    alignItems: "left",
-    backgroundColor: theme.palette.background,
-    overflow: "hidden",
-    maxWidth: "100vw", 
-  },
-
-  innerContainer: {
+  fakeSideBar: {
+    backgroundColor: "#c4c4c4",
     width: "100%",
-    // border: "1px solid #292929",
-    // boxSizing: "border-box",
-    alignItems: "stretch",
-    backgroundColor: theme.palette.background,
-    overflow: "hidden", 
-    display: "flex",
-    justifyContent: "center",
+    height: "100%"
+  },
+
+  gridText:{
+    textAlign: "left"
+  },
+
+  
+  gridAlignment: {
+    marginLeft: "10%",
+    marginRight: "10%",
+    display: "grid",
+    gridTemplateColumns: "20% 80%",
+    gridTemplateAreas: ` 'sideBar header'
+                          'sideBar form' `,
+             
+  },
+
+  dividerBar: {
+    height: "1px",
+    background: "#808080",
+    flexGrow: 1,
   },
 
   headingContainer: {
-    // border: "1px solid #292929",
-    // boxSizing: "border-box",
+    gridArea: "header",
+    //border: "1px solid #03fc17",
+    //boxSizing: "border-box",
     textAlign: "left",
-    backgroundColor: theme.palette.background,
   },
 
-  formContainer: {
-    backgroundColor: "white",
-    paddingRight: "1%",
-    paddingTop: "2%",
-    paddingBottom: "3%"
+  sideBarContainer: {
+    gridArea: "sideBar",
+    //border: "1px solid #292929",
+    //boxSizing: "border-box",
+    width: "100%",
+    //display: "inline"
   },
 
   rightContainer: {
+    gridArea: "form",
   //contains the heading and the form 
-    width: "100%",
     backgroundColor: "white",
-    // border: "1px solid #292929",
-    // boxSizing: "border-box",
-    marginLeft: "5%",
-    //paddingRight: "1%"
+    paddingTop: "5%",
+    paddingBottom: "5%"
+    //border: "1px solid #292929",
+    //boxSizing: "border-box",
+    //marginTop: "20px",
+    //marginBottom: "6 rem",
+    //marginRight: "200px"
   },
 }))
 export default function NewProjectForm() { 
@@ -95,145 +78,80 @@ export default function NewProjectForm() {
 
       <React.Fragment>
         <CssBaseline />
-          <Container className = {classes.pageContainer}>
-            <Container disableGutters = {true} className={classes.innerContainer}>
-            <div className={classes.root}>
-              <Grid container spacing={0}>
-                {/**this is the sidebar */}
-                <Grid item xs={2}>
-                <Paper elevation = {0} className={classes.outerPaper}>
-                  <SideBar/>
-                </Paper>
-                </Grid>
-                {/**This is the form with header */}
-              <Grid item xs = {8}>
-              <Paper elevation = {0} className={classes.outerPaper}>
-              <Container disableGutters = {true} className= {classes.rightContainer}>
-                <div className={classes.root}>
-                <Grid id = "row" container spacing = {24}>
-                    <Container disableGutters = {true} className = {classes.headingContainer}> 
-                      <Grid alignContent = "flex-start" item xs={12}>
-                        <Paper elevation = {0} className={classes.headerPaper}>
-                          <h1>Create Project</h1>
-                          <Divider classes={{root: classes.dividerColor}} variant="fullwidth"/>
-                        </Paper>
-                      </Grid>
-                    </Container>
-                    <Container disableGutters = {true} className = {classes.formContainer}>
-                    <Grid id = "row" container spacing = {24}>
-                    <Grid item xs = {12} sm = {3}>
-                      <Paper elevation = {0} className={classes.paper}>
-                        Project Name:
-                      </Paper>
-                    </Grid>
-                    <Grid item xs = {12} sm = {9}>
-                      <Paper elevation = {0} className={classes.paper}>
-                        <TextField placeholder = "Enter Your Project's Name"/>
-                      </Paper>
-                    </Grid>
-                    <Grid item xs = {12} sm = {3}>
-                      <Paper elevation = {0} className={classes.paper}>
-                        Project Description:
-                      </Paper>
-                    </Grid>
-                    <Grid item xs = {12} sm = {9}>
-                      <Paper elevation = {0} className={classes.paper}>
-                        <TextField placeholder = "Tell us about your project! Be sure to include details of your organization, requirements for potential members, and any further suggestions" 
-                        rows = "15" columns = "30" multiline = {true}/>
-                      </Paper>
-                    </Grid>
-                    <Grid item xs = {12} sm = {3}>
-                      <Paper elevation = {0} className={classes.paper}> 
-                        Due Date: 
-                      </Paper>
-                    </Grid>
-                    <Grid item xs = {12} sm = {9}>
-                      <Paper elevation = {0} className={classes.paper}>
-                        <TextField fullWidth = {false} type = "date"/>
-                      </Paper>
-                    </Grid>
-                    <Grid item xs = {12} sm = {3}>
-                      <Paper elevation = {0} className={classes.paper}>
-                        Attachments:
-                      </Paper>
-                    </Grid>
-                    <Grid item xs = {12} sm = {9}>
-                      <Paper elevation = {0} className={classes.paper}> 
-                        <Button variant="contained" component="label" color = "primary"> 
-                          Upload File
-                          <input type="file" style={{ display: "none" }}/>
-                        </Button>
-                      </Paper>
-                    </Grid>
-                    <Grid item xs = {12} sm = {12}>
-                      <Paper elevation = {0} className={classes.paper}> 
-                        <Divider variant="fullwidth"/>
-                        <h2>Contact Information</h2>
-                      </Paper>
-                    </Grid>
-                    <Grid item xs = {12} sm = {3}>
-                      <Paper elevation = {0} className={classes.paper}> 
-                        Name: 
-                      </Paper> 
-                    </Grid>
-                    <Grid item xs = {12} sm = {9}>
-                      <Paper elevation = {0} className={classes.paper}> 
-                        <TextField placeholder = "No name has been given" />
-                      </Paper> 
-                    </Grid>
-                    <Grid item xs = {12} sm = {3}>
-                      <Paper elevation = {0} className={classes.paper}> 
-                        Phone (optional): 
-                      </Paper>
-                    </Grid>
-                    <Grid item xs = {12} sm = {9}>
-                        <Paper elevation = {0} className={classes.paper}> 
-                          <TextField placeholder = "No phone number has been given"/>              
-                        </Paper>
-                    </Grid>
-                    <Grid item xs = {12} sm = {3}>
-                      <Paper elevation = {0} className={classes.paper}> 
-                        Email:
-                      </Paper>
-                    </Grid>
-                    <Grid item xs = {12} sm = {9}>
-                      <Paper elevation = {0} className={classes.paper}> 
-                        <TextField placeholder = "No email has been given"/>              
-                      </Paper>
-                    </Grid>
-                    <Grid item xs = {12} sm = {3}>
-                      <Paper elevation = {0} className={classes.paper}> 
-                        Primary organization:
-                      </Paper>
-                    </Grid>
-                    <Grid item xs = {12} sm = {9}>
-                      <Paper elevation = {0} className={classes.paper}>
-                        <TextField placeholder = "No organization has been given"/> 
-                      </Paper>
-                    </Grid>
-                    <Grid item xs = {12} sm = {5}>
-                      <Paper elevation = {0} className={classes.paper}></Paper>
-                    </Grid>
-                    <Grid item xs = {12} sm = {7}>
-                      <Paper elevation = {0} className={classes.paper}>
-                        <Button color ="primary" variant = "contained">Submit</Button>
-                      </Paper>
-                    </Grid> 
-                    </Grid>
-                    </Container>
-                  </Grid>
-                </div>
-              </Container>
-              </Paper>
-              </Grid>
-              
-              </Grid>
-              </div>
-            </Container>
+        {/**must wrapper the whole thing as a body? */}
+        <body className = {classes.gridAlignment} >
+          <Container className = {classes.sideBarContainer}>
+            <Box className = {classes.fakeSideBar}></Box>
           </Container>
-        {/* Footer */}
-          <Footer />
-        {/* End footer */}
+              {/*<SideBar/>*/}
+              
+                {/*<div className={classes.root}>*/}
+
+          <Container disableGutters = {true} className = {classes.headingContainer}> 
+              <h1>Create A New Project</h1>
+              <Box className = {classes.dividerBar}></Box>
+          </Container>
+
+          <Container className = {classes.rightContainer}>
+            <Grid container spacing={3} className = {classes.gridText}>
+            <Grid item xs = {12} sm = {12}>
+                <TextField placeholder = "Enter Your Project's Name"/>
+            </Grid>
+            <Grid item xs = {12} sm = {12}>
+                <TextField placeholder = "Tell us about your project! Be sure to include details of your organization, requirements for potential members, and any further suggestions" rows = "20" columns = "30" multiline = {true}/>
+            </Grid>
+            <Grid item xs = {12} sm = {4}>
+                Due Date: 
+            </Grid>
+            <Grid item xs = {12} sm = {8}>
+                <TextField type = "date"/>
+            </Grid>
+            <Grid item xs = {12} sm = {4}>
+            </Grid>
+            <Grid item xs = {12} sm = {8}>
+                <Button variant="contained" component="label" color = "primary"> 
+                  Upload File
+                  <input type="file" style={{ display: "none" }}/>
+                </Button>
+            </Grid>
+            <Grid item xs = {12} sm = {12}>
+                <Divider variant="fullwidth"/>
+                <h2>Contact Information</h2>
+            </Grid>
+            <Grid item xs = {12} sm = {4}>
+                Name: 
+            </Grid>
+            <Grid item xs = {12} sm = {8}>
+                <TextField/>
+            </Grid>
+            <Grid item xs = {12} sm = {4}>
+                Phone(optional): 
+            </Grid>
+            <Grid item xs = {12} sm = {8}>
+                  <TextField/>              
+            </Grid>
+            <Grid item xs = {12} sm = {4}>
+                Email:
+            </Grid>
+            <Grid item xs = {12} sm = {8}>
+                <TextField/>              
+            </Grid>
+            <Grid item xs = {12} sm = {4}>
+                Primary organization:
+            </Grid>
+            <Grid item xs = {12} sm = {8}>
+                <TextField/> 
+            </Grid>
+            
+            <Grid item xs={12}
+                      display="flex"
+                      justify="flex-end">
+                <Button color ="primary" variant = "contained">Submit</Button>
+            </Grid> 
+          </Grid>
+        </Container>
+      {/** </div> */}  
+        </body>  
       </React.Fragment>
     );
   }

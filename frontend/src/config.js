29eda9e53b530b5e1020d5f9e300d80a09624fb2
sarah lum <petrichormist581@gmail.com>
2020-11-13 09:51:32 -0800

    added import statement to landingpage

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  BACKEND_URL: "http://ec2-18-191-183-102.us-east-2.compute.amazonaws.com:4000/" || "http://localhost:4000/"
+  BACKEND_URL: "http://ec2-18-191-183-102.us-east-2.compute.amazonaws.com:4000" || "http://localhost:4000/"
   //process.env.REACT_APP_BACKEND_URL || "http://localhost:4000/",
   //REMEMBER TO CHANGE IT BACK BEFORE YOU MAKE ANY TYPES OF PUSHES
 };

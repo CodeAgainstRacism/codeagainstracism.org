13236123ddab3dd1257937e2e2ed67cb29b16931
lumberry <lumma@uci.edu>
2020-11-14 16:03:29 -0800

    env file + routerLink import

--- frontend/src/pages/LandingPage.jsx
+++ frontend/src/pages/LandingPage.jsx
@@ -1,4 +1,5 @@
-import React, { useEffect, useState } from "react";
+import React,{ useEffect, useState, } from "react";
+import { withRouter, Link as RouterLink } from "react-router-dom";
 import { BACKEND_URL } from "../config";
 import {
   makeStyles,

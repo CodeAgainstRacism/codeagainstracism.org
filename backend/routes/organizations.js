3d67dc056f43eabec353845516b146e55bc135df
mello-code <48005787+mello-code@users.noreply.github.com>
2020-11-28 23:17:46 -0700

    Anh's codes

--- backend/routes/organizations.js
+++ backend/routes/organizations.js
@@ -6,5 +6,33 @@ const bodyParser = require('body-parser');
 const JSONParser = bodyParser.json();
 
 // Write your CRUD routes here 
+router.post('/', JSONParser, (req, res) => {
+    const { uid, ein, orgname, desc } = req.body;
+    const orgid = firebase.database().ref('organizations').push().key; //getting key
+
+    firebase.database().ref('organizations/' + orgid).set({
+    id: orgid,
+    EIN: ein,
+    orgName: orgname,
+    description: desc,
+    projects:
+    [
+
+    ],
+    team:
+    [
+
+    ],
+    useraccount: uid
+  }).then( () => {
+    res.json({orgid})
+    
+  }).catch((error) => {
+    res.json({
+      code: error.code,
+      message: error.message,
+    });
+  });
+});
 
 module.exports = router;

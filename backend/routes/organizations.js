853d63869b27e21a57b07457057e4c74fa4108b4
mello-code <48005787+mello-code@users.noreply.github.com>
2020-12-29 17:09:32 -0700

    Update organizations.js

--- backend/routes/organizations.js
+++ backend/routes/organizations.js
@@ -9,31 +9,28 @@ const descriptionMax = 100;
 const nameMax = 40;
 const usernameMax = 40;
 
-// RUD, no C
+// POST is for testing only
+router.post('/', JSONParser, (req, res) => {
+  const uid = firebase.database().ref('users/organizations').push().key; //getting key
+  const { EIN, orgName, description, email, firstName, lastName, phoneNumber, username } = req.body;
 
-// router.post('/', JSONParser, (req, res) => {
-//   const uid = firebase.database().ref('organizations').push().key; //getting key
-//   const { EIN, orgName, description, email, firstName, lastName, phoneNumber, username } = req.body;
-
-//   firebase.database().ref('organizations/' + orgid).set({
-//     id: uid,
-//     EIN,
-//     orgName,
-//     description,
-//     email,
-//     firstName,
-//     lastName,
-//     phoneNumber,
-//     username
-//   }).then(() => {
-//     res.json({ orgid })
-//   }).catch((error) => {
-//     res.json({
-//       code: error.code,
-//       message: error.message,
-//     });
-//   });
-// });
+  firebase.database().ref('users/organizations/' + uid).set({
+    id: uid,
+    EIN,
+    orgName,
+    description,
+    email,
+    firstName,
+    lastName,
+    phoneNumber,
+    username
+  }).then(() => {
+    res.json({ uid })
+  }).catch((error) => res.json({
+    status: error.code,
+    message: error.message,
+  }));
+});
 
 router.get('/:uid', (req, res) => {
   const { uid } = req.params;
@@ -42,9 +39,12 @@ router.get('/:uid', (req, res) => {
     if (data.val() != null) {
       res.json(Object(data.val()));
     } else {
-      res.json({ err: 'Organization does not exist' });
+      res.json({ error: 'Organization does not exist' });
     }
-  }).catch((err) => res.json({ err: err.message }));
+  }).catch((error) => res.json({
+    status: error.code,
+    message: error.message,
+  }));
 });
 
 router.patch('/:uid', (req, res) => {
@@ -62,27 +62,29 @@ router.patch('/:uid', (req, res) => {
     phoneNumber,
     username
   }).then(() => {
-    if (typeof(EIN) != 'number' || (EIN < 100000000 && EIN >= 1000000000)) {
+    if (typeof (EIN) != 'number' || EIN < 100000000 || EIN >= 1000000000) {
       res.json({ status: 400, message: 'EIN must be a 9 digit number.' })
-    } else if (typeof(orgName) != 'string' || orgName.length > orgNameMax) {
+    } else if (typeof (orgName) != 'string' || orgName.length > orgNameMax) {
       res.json({ status: 400, message: `Organization name must be a string with less than ${orgNameMax} characters.` })
-    } else if (typeof(description) != 'string' || description.length > descriptionMax) {
+    } else if (typeof (description) != 'string' || description.length > descriptionMax) {
       res.json({ status: 400, message: `Organization name must be a string with less than ${descriptionMax} characters.` })
-    } else if (typeof(email) != 'string' || /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(email) == false) {
+    } else if (typeof (email) != 'string' || /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(email) == false) {
       res.json({ status: 400, message: 'Please enter a valid email address.' })
-    } else if (typeof(firstName) != 'string' || firstName.length > nameMax) {
+    } else if (typeof (firstName) != 'string' || firstName.length > nameMax) {
       res.json({ status: 400, message: `First name must be a string with less than ${nameMax} characters.` })
-    } else if (typeof(lastName) != 'string' || lastName.length > nameMax) {
+    } else if (typeof (lastName) != 'string' || lastName.length > nameMax) {
       res.json({ status: 400, message: `Last name must be a string with less than ${nameMax} characters.` })
-    } else if (typeof(phoneNumber) != 'string' || /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/.test(phoneNumber) == false) {
+    } else if (typeof (phoneNumber) != 'string' || /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/.test(phoneNumber) == false) {
       res.json({ status: 400, message: 'Please enter a valid US phone number. It can be either formatted as: XXX-XXX-XXXX, XXX.XXX.XXXX or XXX XXX XXXX.' })
-    } else if (typeof(username) != 'string' || username.length > usernameMax) {
+    } else if (typeof (username) != 'string' || username.length > usernameMax) {
       res.json({ status: 400, message: `Username must be a string with less than ${usernameMax} characters.` })
     } else {
       res.json({ status: 200, message: 'Successfully updated' })
     }
-  }
-  ).catch((err) => res.json({ err: err.message }));
+  }).catch((error) => res.json({
+    status: error.code,
+    message: error.message
+  }));
 });
 
 router.delete('/:uid', (req, res) => {
@@ -90,7 +92,10 @@ router.delete('/:uid', (req, res) => {
   firebase.database().ref('users/organizations/' + uid).remove(
   ).then(() =>
     res.json({ status: 200, message: 'Successfully deleted' })
-  ).catch((err) => res.json({ error: err.message }));
+  ).catch((error) => res.json({
+    status: error.code,
+    message: error.message,
+  }));
 });
 
 module.exports = router;
